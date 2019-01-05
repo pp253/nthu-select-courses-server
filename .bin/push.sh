16cacb5aa05d85b3c30e0e243e99e124fb64a7ef
pp253 <pp.pp253@gmail.com>
2019-01-05 23:49:40 +0800

    fix push and select-course source

--- .bin/push.sh
+++ .bin/push.sh
@@ -2,4 +2,8 @@
 host='140.114.212.43'
 username='linaro'
 
+sftp $username@$host < ./.bin/script-ftp.sh
+
 cat ./.bin/script.sh | ssh $username@$host
+
+echo "Connecting to $host via sftp."
---
+++ .bin/script-ftp.sh
@@ -0,0 +1,3 @@
+cd ./Documents/Coding/nthu-select-courses-server/src/api
+put ./src/api/courses_db.json
+exit
