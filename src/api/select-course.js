f29ea7e1efbf13aab2d2e872b56bb845280a2fed
pp253 <pp.pp253@gmail.com>
2019-01-06 00:51:14 +0800

    fix error typo

--- src/api/select-course.js
+++ src/api/select-course.js
@@ -300,7 +300,7 @@ export function addCourse(sessionToken, courseNumber, order = '') {
             reject(response.ResponseErrorMsg.NotAvailable())
             return
           } else if (body.startsWith(config.grabdata.errGeneralCoursesNotMoreThanThree)) {
-            reject(response.ResponseErrorMsg.NotAvailable())
+            reject(response.ResponseErrorMsg.errGeneralCoursesNotMoreThanThree())
             return
           } else if (body.startsWith(config.grabdata.errNotValid)) {
             reject(response.ResponseErrorMsg.NotValid())
