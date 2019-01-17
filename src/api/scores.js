e98780f919e18b59cc28c20f1d6c729fe73ae2da
pp253 <pp.pp253@gmail.com>
2019-01-18 04:07:38 +0800

    lint

--- src/api/scores.js
+++ src/api/scores.js
@@ -93,93 +93,157 @@ import grabHelper from './grab-helper'
      }
    }
  */
-export function getScores (sessionToken) {
+export function getScores(sessionToken) {
   return new Promise((resolve, reject) => {
     correctRequest({
       url: config.grabdata.scoresPage.replace('{0}', sessionToken),
       method: 'POST'
     })
-    .then((body) => {
-      if (body === config.grabdata.errSessionInterrupted) {
-        reject(response.ResponseErrorMsg.SessionInterrupted())
-        return
-      }
-
-      const $ = cheerio.load(body)
-      let scores = {}
-      let courses = {}
-      let table = $('table')
-      let table1 = table.get(1).children[1].children // table[1] > tbody.children
-
-      for (let trIdx in table1) {
-        let tr = table1[trIdx]
-        if (tr.type === 'text') {
-          continue
-        }
-        if (tr.children[1].type !== 'text' && tr.children[1].attribs.class !== 'input') {
-          continue
+      .then(body => {
+        if (body === config.grabdata.errSessionInterrupted) {
+          reject(response.ResponseErrorMsg.SessionInterrupted())
+          return
         }
 
-        let semester = $(tr.children[1].children[0]).text().trim() + $(tr.children[3].children[0]).text().trim()
-        let shortCourseNumber = $(tr.children[5].children[0]).text().trim()
+        const $ = cheerio.load(body)
+        let scores = {}
+        let courses = {}
+        let table = $('table')
+        let table1 = table.get(1).children[1].children // table[1] > tbody.children
 
-        let score = {
-          semester: semester,
-          courseNumber: semester + shortCourseNumber,
-          courseTitle: $(tr.children[7].children[0]).text().trim(),
-          credit: $(tr.children[9].children[0]).text().trim(),
-          grade: tr.children[11].children[0] && $(tr.children[11].children[0]).text().trim()
+        for (let trIdx in table1) {
+          let tr = table1[trIdx]
+          if (tr.type === 'text') {
+            continue
+          }
+          if (
+            tr.children[1].type !== 'text' &&
+            tr.children[1].attribs.class !== 'input'
+          ) {
+            continue
+          }
+
+          let semester =
+            $(tr.children[1].children[0])
+              .text()
+              .trim() +
+            $(tr.children[3].children[0])
+              .text()
+              .trim()
+          let shortCourseNumber = $(tr.children[5].children[0])
+            .text()
+            .trim()
+
+          let score = {
+            semester: semester,
+            courseNumber: semester + shortCourseNumber,
+            courseTitle: $(tr.children[7].children[0])
+              .text()
+              .trim(),
+            credit: $(tr.children[9].children[0])
+              .text()
+              .trim(),
+            grade:
+              tr.children[11].children[0] &&
+              $(tr.children[11].children[0])
+                .text()
+                .trim()
+          }
+
+          if (!scores[score.semester]) {
+            scores[score.semester] = []
+          }
+          scores[score.semester].push(score.courseNumber)
+          courses[score.courseNumber] = score
         }
 
-        if (!scores[score.semester]) {
-          scores[score.semester] = []
+        // table[4] > tbody.children
+        let table4 = $(table.get(4))
+          .find('tr')
+          .toArray()
+        table4.splice(0, 2)
+        let overview = {}
+        for (let tr of table4) {
+          let semester =
+            $(tr.children[1].children[0])
+              .text()
+              .trim() +
+            $(tr.children[3].children[0])
+              .text()
+              .trim()
+          let gpa =
+            tr.children[5].children[0] &&
+            $(tr.children[5].children[0])
+              .text()
+              .trim()
+          gpa = gpa === '-' ? '' : gpa
+          let credit =
+            tr.children[7].children[0].type === 'tag'
+              ? ''
+              : $(tr.children[7].children[0])
+                  .text()
+                  .trim()
+          let deservedCredit =
+            tr.children[9].children[0].type === 'tag'
+              ? ''
+              : $(tr.children[9].children[0])
+                  .text()
+                  .trim()
+          let courses =
+            tr.children[11].children[0].type === 'tag'
+              ? ''
+              : $(tr.children[11].children[0])
+                  .text()
+                  .trim()
+          let summerVacationCredit =
+            tr.children[13].children[0].type === 'tag'
+              ? ''
+              : $(tr.children[13].children[0])
+                  .text()
+                  .trim()
+          let transferCredit =
+            tr.children[15].children[0].type === 'tag'
+              ? ''
+              : $(tr.children[15].children[0])
+                  .text()
+                  .trim()
+          let classRanking = $(tr.children[17].children[0])
+            .text()
+            .trim()
+          classRanking = classRanking === '-' ? '' : classRanking
+          let departmentRanking = $(tr.children[19].children[0])
+            .text()
+            .trim()
+          departmentRanking = departmentRanking === '-' ? '' : departmentRanking
+          let comments = $(tr.children[21].children[0])
+            .text()
+            .trim()
+
+          overview[semester] = {
+            semester,
+            gpa,
+            credit,
+            deservedCredit,
+            courses,
+            summerVacationCredit,
+            transferCredit,
+            classRanking,
+            departmentRanking,
+            comments
+          }
         }
-        scores[score.semester].push(score.courseNumber)
-        courses[score.courseNumber] = score
-      }
 
-      // table[4] > tbody.children
-      let table4 = $(table.get(4)).find('tr').toArray()
-      table4.splice(0, 2)
-      let overview = {}
-      for (let tr of table4) {
-        let semester = $(tr.children[1].children[0]).text().trim() + $(tr.children[3].children[0]).text().trim()
-        let gpa = tr.children[5].children[0] && $(tr.children[5].children[0]).text().trim()
-        gpa = gpa === '-' ? '' : gpa
-        let credit = tr.children[7].children[0].type === 'tag' ? '' : $(tr.children[7].children[0]).text().trim()
-        let deservedCredit = tr.children[9].children[0].type === 'tag' ? '' : $(tr.children[9].children[0]).text().trim()
-        let courses = tr.children[11].children[0].type === 'tag' ? '' : $(tr.children[11].children[0]).text().trim()
-        let summerVacationCredit = tr.children[13].children[0].type === 'tag' ? '' : $(tr.children[13].children[0]).text().trim()
-        let transferCredit = tr.children[15].children[0].type === 'tag' ? '' : $(tr.children[15].children[0]).text().trim()
-        let classRanking = $(tr.children[17].children[0]).text().trim()
-        classRanking = classRanking === '-' ? '' : classRanking
-        let departmentRanking = $(tr.children[19].children[0]).text().trim()
-        departmentRanking = departmentRanking === '-' ? '' : departmentRanking
-        let comments = $(tr.children[21].children[0]).text().trim()
-
-        overview[semester] = {
-          semester,
-          gpa,
-          credit,
-          deservedCredit,
-          courses,
-          summerVacationCredit,
-          transferCredit,
-          classRanking,
-          departmentRanking,
-          comments
-        }
-      }
-
-      resolve(response.ResponseSuccessJSON({
-        scores: scores,
-        courses: courses,
-        overview: overview
-      }))
-    })
-    .catch((err) => {
-      reject(err)
-    })
+        resolve(
+          response.ResponseSuccessJSON({
+            scores: scores,
+            courses: courses,
+            overview: overview
+          })
+        )
+      })
+      .catch(err => {
+        reject(err)
+      })
   })
 }
 
@@ -221,44 +285,60 @@ export function getScores (sessionToken) {
      }
    }
  */
-export function getDistribution (sessionToken, courseNumber) {
+export function getDistribution(sessionToken, courseNumber) {
   return new Promise((resolve, reject) => {
     correctRequest({
-      url: config.grabdata.distributionPage.replace('{0}', sessionToken).replace('{1}', courseNumber)
+      url: config.grabdata.distributionPage
+        .replace('{0}', sessionToken)
+        .replace('{1}', courseNumber)
     })
-    .then((body) => {
-      if (body === config.grabdata.errSessionInterrupted) {
-        reject(response.ResponseErrorMsg.SessionInterrupted())
-        return
-      }
+      .then(body => {
+        if (body === config.grabdata.errSessionInterrupted) {
+          reject(response.ResponseErrorMsg.SessionInterrupted())
+          return
+        }
 
-      const $ = cheerio.load(body)
-      let tdArray = $('table table tbody tr:nth-child(2) td').toArray()
-      let distribution = {}
-      let gradeList = [
-        'A+',
-        'A',
-        'A-',
-        'B+',
-        'B',
-        'B-',
-        'C+',
-        'C',
-        'C-',
-        'D',
-        'E',
-        'X',
-        'not_yet',
-        'total'
-      ]
+        const $ = cheerio.load(body)
+        let tdArray = $('table table tbody tr:nth-child(2) td').toArray()
+        let distribution = {}
+        let gradeList = [
+          'A+',
+          'A',
+          'A-',
+          'B+',
+          'B',
+          'B-',
+          'C+',
+          'C',
+          'C-',
+          'D',
+          'E',
+          'X',
+          'not_yet',
+          'total'
+        ]
 
-      for (let idx = 0; idx < gradeList.length; idx++) {
-        let peopleNum = /\((\d+)人\)/.exec($(tdArray[idx + 1].children[2]).text().trim())
-        distribution[gradeList[idx]] = (peopleNum && peopleNum.length === 2) ? parseInt(peopleNum[1]) : 0
-      }
+        for (let idx = 0; idx < gradeList.length; idx++) {
+          let peopleNum = /\((\d+)人\)/.exec(
+            $(tdArray[idx + 1].children[2])
+              .text()
+              .trim()
+          )
+          distribution[gradeList[idx]] =
+            peopleNum && peopleNum.length === 2 ? parseInt(peopleNum[1]) : 0
+        }
 
-      resolve(response.ResponseSuccessJSON({
-        distribution: distribution
+        resolve(
+          response.ResponseSuccessJSON({
+            distribution: distribution
+          })
+        )
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
 
 export function getClassmates(sessionToken, courseNumber) {
   return new Promise((resolve, reject) => {
@@ -349,9 +429,9 @@ export function getClassmates(sessionToken, courseNumber) {
             courseNumber: courseNumber
           })
         )
-    })
+      })
       .catch(err => {
-      reject(err)
-    })
+        reject(err)
+      })
   })
 }
